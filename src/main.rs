b133395cab06a64496b34778fdcf9008d56acb6f
Chris <8133496+chriswmann@users.noreply.github.com>
2025-07-05 10:22:41 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,7 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
     match pattern {
         p if p.len() == 1 => input_line.contains(p),
         r"\d" => input_line.chars().any(|char| char.is_ascii_digit()),
-        r"\w" => input_line
-            .chars()
-            .any(|char| char.is_alphanumeric() || char == '_'),
+        r"\w" => input_line.chars().any(|char| char.is_alphanumeric()),
         _ => false,
     }
 }
